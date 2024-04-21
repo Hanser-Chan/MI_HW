63776d8de8096f5f2880de1b5afc58e9ce140497
ChenJinjian <852627653cjj@gmail.com>
2024-04-21 14:11:05 +0800

    ex3

--- Day02/exercise3/max.cpp
+++ Day02/exercise3/max.cpp
@@ -7,10 +7,15 @@ int smax(int a, int b, int c)
     return std::max({a, b, c});
 }
 
+int smax(int a, int b)
+{
+    return std::max(a, b);
+}
+
 int main()
 {
     int a{}, b{}, c{};
-    std::cout << "请输入3个数: ";
+    std::cout << "请输入2~3个数: ";
     std::cin >> a >> b >> c;
     std::cout << std::format("3个数中最大值为{}", smax(a, b, c)) << std::endl;
 }
