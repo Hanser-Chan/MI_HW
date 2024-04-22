3c3567bc7c20f7124dae750c107fb4152642dae7
ChenJinjian <852627653cjj@gmail.com>
2024-04-22 10:59:56 +0800

    add note

--- Day03/exercise1/add.cpp
+++ Day03/exercise1/add.cpp
@@ -8,11 +8,18 @@ int add(int a, int b)
 int main()
 {
 
-    int (*pf)(int a, int b) = add;
+    int (*pf)(int a, int b) = add; // 解引用调用
 
     std::cout << "(*pf)(1, 2) = " << (*pf)(1, 2) << std::endl;
 
-    pf = &add;
+    pf = &add; // 直接调用
 
     std::cout << "pf(1, 2) = " << pf(1, 2) << std::endl;
+
+    std::cout << (void *)add << std::endl
+              << (void *)*add << std::endl
+              << &add << std::endl
+              << pf << std::endl
+              << *pf << std::endl
+              << &pf << std::endl;
 }
