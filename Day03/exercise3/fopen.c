2df5d468774c8640bac7f7edba6d22a72033ac48
ChenJinjian <852627653cjj@gmail.com>
2024-04-22 15:58:24 +0800

    I love Mr.Lei

--- Day03/exercise3/fopen.c
+++ Day03/exercise3/fopen.c
@@ -5,18 +5,19 @@ FILE *fl;
 int main()
 {
 
-    fl = fopen("/Users/CJJ/Documents/mi_hw/Day03/exercise3/text.txt", "rw");
+    fl = fopen("/Users/CJJ/Documents/mi_hw/Day03/exercise3/text.txt", "a+");
     if (fl == NULL)
     {
         printf("文件打开失败");
         return 1;
     }
 
-    char input[100];
+    // char input[] = {'c', 'h', 'e', 'n', 'j', 'i', 'n', 'j', 'i', 'a', 'n'};
 
-    printf("请输入要写入文件的内容（最多100个字符）：\n");
-    fgets(input, sizeof(input), stdin);
-    fputs(input, fl);
+    // printf("请输入要写入文件的内容（最多100个字符）：\n");
+    // fgets(input, sizeof(input), stdin);
+    // fputs(input, fl);
+    fputs("-chenjinjian\n", fl);
 
     printf("内容已成功写入文件。\n");
 
