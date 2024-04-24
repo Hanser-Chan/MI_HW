9b0ab2687a06b7632a49efc9046f99f65151c91a
HanserChan <852627653cjj@gmail.com>
2024-04-24 17:04:13 +0800

    hw1+test3

---
+++ Day05/mutex_test3/test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <future>
+
+void print_int(std::future<int> &fut)
+{
+    int x = fut.get();
+    std::cout << "value: " << x << '\n';
+}
+
+int main()
+{
+    std::promise<int> prom;
+    std::future<int> fut = prom.get_future();
+    std::thread th1(print_int, std::ref(fut));
+
+    prom.set_value(10);
+
+    th1.join();
+
+    return 0;
+}
