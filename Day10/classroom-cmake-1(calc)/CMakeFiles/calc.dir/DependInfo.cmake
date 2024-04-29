499155a048a245af38d592c1a8f666b3ab7f435e
Hanser-Chan <852627653cjj@gmail.com>
2024-04-29 10:50:21 +0800

    ex2

---
+++ Day10/classroom-cmake-1(calc)/CMakeFiles/calc.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu2204/桌面/classroom-cmake-1(calc)/add.cpp" "CMakeFiles/calc.dir/add.cpp.o" "gcc" "CMakeFiles/calc.dir/add.cpp.o.d"
+  "/home/ubuntu2204/桌面/classroom-cmake-1(calc)/division.cpp" "CMakeFiles/calc.dir/division.cpp.o" "gcc" "CMakeFiles/calc.dir/division.cpp.o.d"
+  "/home/ubuntu2204/桌面/classroom-cmake-1(calc)/main.cpp" "CMakeFiles/calc.dir/main.cpp.o" "gcc" "CMakeFiles/calc.dir/main.cpp.o.d"
+  "/home/ubuntu2204/桌面/classroom-cmake-1(calc)/mutiple.cpp" "CMakeFiles/calc.dir/mutiple.cpp.o" "gcc" "CMakeFiles/calc.dir/mutiple.cpp.o.d"
+  "/home/ubuntu2204/桌面/classroom-cmake-1(calc)/sub.cpp" "CMakeFiles/calc.dir/sub.cpp.o" "gcc" "CMakeFiles/calc.dir/sub.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Day10/classroom-cmake-1(calc)/CMakeFiles/calc.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calc.dir/add.cpp.o"
+  "CMakeFiles/calc.dir/add.cpp.o.d"
+  "CMakeFiles/calc.dir/division.cpp.o"
+  "CMakeFiles/calc.dir/division.cpp.o.d"
+  "CMakeFiles/calc.dir/main.cpp.o"
+  "CMakeFiles/calc.dir/main.cpp.o.d"
+  "CMakeFiles/calc.dir/mutiple.cpp.o"
+  "CMakeFiles/calc.dir/mutiple.cpp.o.d"
+  "CMakeFiles/calc.dir/sub.cpp.o"
+  "CMakeFiles/calc.dir/sub.cpp.o.d"
+  "calc"
+  "calc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
