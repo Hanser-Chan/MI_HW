16cc291f44ee5e1b74d069b3b65d3e1333d97a74
Hanser-Chan <852627653cjj@gmail.com>
2024-08-27 17:55:52 +0800

    修正'哲学家进餐'问题死锁情况

--- Day05/HW1/README.md
+++ Day05/HW1/README.md
@@ -1,11 +1,13 @@
-# 说明
-> 由于macOS内置CMake会自动结束线程，转移至windows可以正常输出
+# 哲学家进餐问题说明
+> 由于macOS会自动结束线程，转移至windows可以正常输出
 > 
 > 吃5次情况具体输出可参考`output.pdf`,吃1次如下图
 
 ![philo.png](https://img2.imgtp.com/2024/04/24/eWf45LAl.png)
 
 ## 算法实现
+
+### 牢版本(2024.5)
 
 每个线程会执行抢占执行`上锁`,所以在拿起筷子这里只考虑一个人的行为即可。
 
@@ -33,7 +35,50 @@ while (true){
     }
 ```
 
-## 优化代码
+### 新版本(2024.8)
+
+>在复习操作系统的时候，又把这段代码拿出来看了一下。
+>
+
+上面的代码会出现一个**典型的问题**——如果都同时5个人**并发**拿筷子，就会产生**死锁**，幸运的是，这个问题在运行时出现，但是这个也是需要解决的。
+
+所以我们可以添加一个拿筷子的时候的`mutex`，这样就不会出现五个人同时拿右边筷子的情况。
+
+那如果只是需要不出现`五个人同时拿右边筷子`的情况，我们可以变更某一个人的拿筷子顺序，所以我们只需要把4号的顺序改成先拿左后拿右即可，就不用再设置一个新的`互斥量`。
+
+```cpp
+void eat(int id){
+    int cishu{0};
+    int left_chop = id;
+    int right_chop = (id+1) % PHI;
+    while (true){
+        cishu++;
+        if (cishu > 5) break;
+        cout << format("哲学家{}在思考\n", id);
+        if (id == 4) {  // 更改最后一位哲学家的拿筷子顺序
+            chop[left_chop].lock();
+            cout << format("哲学家{}拿起了左边的筷子{}\n", id, left_chop);
+            chop[right_chop].lock();
+            cout << format("哲学家{}拿起了右边的筷子{}\n", id, right_chop);
+        } else {
+            chop[right_chop].lock();
+            cout << format("哲学家{}拿起了右边的筷子{}\n", id, right_chop);
+            chop[left_chop].lock();
+            cout << format("哲学家{}拿起了左边的筷子{}\n", id, left_chop);
+        }
+        cout << format("哲学家{}吃个饭先,已经吃了{}次\n", id, cishu);
+        std::this_thread::sleep_for(std::chrono::microseconds(1));
+        chop[right_chop].unlock();
+        chop[left_chop].unlock();
+        cout << format("哲学家{}吃爽了，放下了左边{}筷子和右边{}筷子\n", id, left_chop, right_chop);
+    }
+}
+
+```
+
+
+
+## 管理线程
 
 使用`std::vector`存储不同的线程,再由循环统一结束执行。可以省去很多的定义代码部分。
 ```c++
--- Day05/HW1/main.cpp
+++ Day05/HW1/main.cpp
@@ -10,48 +10,62 @@ const int PHI {5};
 
 std::mutex chop[PHI];
 
+//void eat(int id){
+//    int cishu{0};
+//
+//    int left_chop{id};
+//    int right_chop{(id+1) % PHI};
+//
+//    while (true){
+//        cishu++;
+//        if (cishu > 5) break;
+//        cout << format("哲学家{}在思考\n",id);
+//
+//        chop[right_chop].lock();
+//        cout << format("哲学家{}拿起了右边的筷子{}\n",id,right_chop);
+//
+//        chop[left_chop].lock();
+//        cout << format("哲学家{}拿起了左边的筷子{}\n",id,left_chop);
+//
+//        cout << format("哲学家{}吃个饭先,已经吃了{}次\n",id,cishu);
+//        std::this_thread::sleep_for(std::chrono::microseconds(1));
+//
+//        chop[right_chop].unlock();
+//        chop[left_chop].unlock();
+//
+//        cout << format("哲学家{}吃爽了，放下了左边{}筷子和右边{}筷子\n",id,left_chop,right_chop);
+//    }
+//}
+
 void eat(int id){
     int cishu{0};
-
-    int left_chop{id};
-    int right_chop{(id+1) % PHI};
-
+    int left_chop = id;
+    int right_chop = (id+1) % PHI;
     while (true){
         cishu++;
         if (cishu > 5) break;
-        cout << format("哲学家{}在思考\n",id);
-
-        chop[right_chop].lock();
-        cout << format("哲学家{}拿起了右边的筷子{}\n",id,right_chop);
-
-        chop[left_chop].lock();
-        cout << format("哲学家{}拿起了左边的筷子{}\n",id,left_chop);
-
-        cout << format("哲学家{}吃个饭先,已经吃了{}次\n",id,cishu);
+        cout << format("哲学家{}在思考\n", id);
+        if (id == 4) {  // 更改最后一位哲学家的拿筷子顺序
+            chop[left_chop].lock();
+            cout << format("哲学家{}拿起了左边的筷子{}\n", id, left_chop);
+            chop[right_chop].lock();
+            cout << format("哲学家{}拿起了右边的筷子{}\n", id, right_chop);
+        } else {
+            chop[right_chop].lock();
+            cout << format("哲学家{}拿起了右边的筷子{}\n", id, right_chop);
+            chop[left_chop].lock();
+            cout << format("哲学家{}拿起了左边的筷子{}\n", id, left_chop);
+        }
+        cout << format("哲学家{}吃个饭先,已经吃了{}次\n", id, cishu);
         std::this_thread::sleep_for(std::chrono::microseconds(1));
-
         chop[right_chop].unlock();
         chop[left_chop].unlock();
-
-        cout << format("哲学家{}吃爽了，放下了左边{}筷子和右边{}筷子\n",id,left_chop,right_chop);
+        cout << format("哲学家{}吃爽了，放下了左边{}筷子和右边{}筷子\n", id, left_chop, right_chop);
     }
 }
 
+
 int main() {
-//    std::thread th1(eat,1);
-//    std::thread th2(eat,2);
-//    std::thread th3(eat,3);
-//    std::thread th4(eat,4);
-//    std::thread th5(eat,5);
-//
-//
-//    if(th1.joinable() && th2.joinable() && th3.joinable() && th4.joinable() && th5.joinable()){
-//        th1.join();
-//        th2.join();
-//        th3.join();
-//        th4.join();
-//        th5.join();
-//    }
 
     std::vector<std::thread> philo;
 
